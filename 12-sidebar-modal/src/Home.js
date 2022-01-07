2a951974767078e8119f524b513d87368b0cfea8
bhimani-akash <bhimani.a@northeastern.edu>
2022-01-07 15:53:21 -0500

    completed sidebar-modal applicaiton

--- 12-sidebar-modal/src/Home.js
+++ 12-sidebar-modal/src/Home.js
@@ -1,8 +1,19 @@
-import React, { useContext } from 'react'
-import { FaBars } from 'react-icons/fa'
+import React, { useContext } from "react";
+import { FaBars } from "react-icons/fa";
+import { AppContext, useGlobalContext } from "./context";
 
 const Home = () => {
-  return <h2>home component</h2>
-}
+  const { openSidebar, openModal } = useGlobalContext();
+  return (
+    <main>
+      <button className="sidebar-toggle" onClick={openSidebar}>
+        <FaBars />
+      </button>
+      <button className="btn" onClick={openModal}>
+        show modal
+      </button>
+    </main>
+  );
+};
 
-export default Home
+export default Home;
